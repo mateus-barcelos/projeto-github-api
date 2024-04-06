a551e8719c622826aa1d404a4d922a08ca57f307
Mateus Barcelos <chefinho2832@gmail.com>
2024-04-06 18:54:04 -0300

    adicionado validações de input vazio e not found user

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,6 +6,8 @@ import { screen } from './objects/screen.js'
 
 document.getElementById('btn-search').addEventListener('click', () => {
     let userName = document.getElementById('input-search').value
+    if (validateEmptyInput(userName)) return
+
     getUserData(userName)
 })
 
@@ -14,6 +16,8 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
     const key = e.which || e.keyCode
     const isEnterKeyPressed = key === 13
 
+    if (validateEmptyInput(userName)) return
+
     if (isEnterKeyPressed) getUserData(userName)
 })
 
@@ -21,9 +25,20 @@ async function getUserData(userName) {
     const userResponse = await getUser(userName)
     const repositoriesResponse = await getRepositories(userName)
 
+    if(userResponse.message === "Not Found"){
+        screen.renderNotFound()
+        return
+    }
+
     user.setInfo(userResponse)
     user.setRepositories(repositoriesResponse)
-
     screen.renderUser(user)
 
+}
+
+function validateEmptyInput(userName) {
+    if (userName.length === 0) {
+        alert('Insira um nome de usuário do GitHub')
+        return true
+    }
 }
